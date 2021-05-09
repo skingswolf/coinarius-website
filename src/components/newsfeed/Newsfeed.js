761074678a80f7d2800fef2dfc37df8d93faf5db
Steven Kingswolf <stevenkingswolf@gmail.com>
2021-05-09 10:40:45 +0100

    Implement skeleton structure of newsfeed items

--- src/components/newsfeed/Newsfeed.js
+++ src/components/newsfeed/Newsfeed.js
@@ -1,5 +1,26 @@
 import React from "react";
+import styled from "styled-components";
 
-const Newsfeed = () => <div>Newsfeed</div>;
+import newsStories from "./newsStories";
+
+const StyledNewsStory = styled.div`
+  border: red solid 1px;
+  margin: 15px 15px 0 15px;
+  padding: 15px;
+`;
+
+const newsfeedItems = newsStories.map((newsStory) => (
+  <StyledNewsStory>
+    <div>
+      <div>{newsStory.title}</div>
+      <div>
+        <span>{newsStory.publisher}</span>
+        <span>{newsStory.time}</span>
+      </div>
+    </div>
+  </StyledNewsStory>
+));
+
+const Newsfeed = () => newsfeedItems;
 
 export default Newsfeed;
