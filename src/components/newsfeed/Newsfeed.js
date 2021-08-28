33811bf16be98e34a70e635dc4afeddffc2014a5
Steven Kingswolf <stevenkingswolf@gmail.com>
2021-08-28 21:13:55 +0100

    Link up news-stories UI panel with coinarius-nlp API and WebSocket service

--- src/components/newsfeed/Newsfeed.js
+++ src/components/newsfeed/Newsfeed.js
@@ -1,4 +1,5 @@
 import { css } from "@emotion/react";
+import io from "socket.io-client";
 import React, { useEffect, useState } from "react";
 import MoonLoader from "react-spinners/MoonLoader";
 import styled from "styled-components";
@@ -30,25 +31,27 @@ const getNewsfeedItems = (newsStories) => {
   ));
 };
 
+const nlpAnalyticsUrl = "https://coinarius-nlp.herokuapp.com";
+let socket = null;
+
 const Newsfeed = () => {
   const [news, setNews] = useState({
     isLoading: true,
     stories: null
   });
 
+  // Fetch data from /news-stories endpoint.
   useEffect(() => {
-    const url = "https://coinarius-nlp.herokuapp.com/news-stories";
+    const newsStoriesUrl = `${nlpAnalyticsUrl}/news-stories`;
 
     const fetchData = async () => {
       try {
-        const response = await fetch(url);
-        const json = await response.json();
-
-        console.log(json);
+        const response = await fetch(newsStoriesUrl);
+        const jsonResponse = await response.json();
 
         setNews({
           isLoading: false,
-          stories: json.news_stories
+          stories: jsonResponse.news_stories
         });
       } catch (error) {
         console.log("error", error);
@@ -56,6 +59,33 @@ const Newsfeed = () => {
     };
 
     fetchData();
+  }, []);
+
+  // Connect and fetch data from coinarius-nlp WebSocket service.
+  useEffect(() => {
+    console.log("Initialising socket.");
+    socket = io(nlpAnalyticsUrl);
+    socket.on("connect", () => {
+      console.log("Successfully connected to Coinarius NLP WebSocket service.");
+      socket.emit("register", { data: "register" });
+    });
+
+    socket.on(
+      "fresh_nlp_analytics",
+      (msg) => {
+        console.log("Fresh data received from Coinarius NLP WebSocket service.");
+        const updatedNewsStories = msg.news_stories;
+
+        setNews((n) => ({ ...n, stories: updatedNewsStories.concat(n.stories) }));
+      },
+      []
+    );
+
+    // eslint-disable-next-line consistent-return
+    return () => {
+      console.log("Disconnecting from Coinarius NLP WebSocket service.");
+      socket.disconnect();
+    };
   }, []);
 
   return news.isLoading ? (
